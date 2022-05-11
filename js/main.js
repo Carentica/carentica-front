a050b164c408fc23fedfb1d881220e3064a2fc03
Layronne <92851704+Layronne@users.noreply.github.com>
2022-05-11 10:47:14 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const btnMenu = document.getElementById("btnMenu");
 const svgMenu = document.querySelectorAll("#btnMenu svg");
+const test = "test";
 
 const mobileMenu = document.getElementById("mobileMenu");
 btnMenu.addEventListener('click', (e) => {
