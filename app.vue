a899122513469d2ca9655af75c663e93eb847e55
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-01-30 12:48:31 +0100

    uninstall tailwind

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold text-green-500">
-      How to install Tailwind CSS 3 in Nuxt 3
-    </h1>
+    Test
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default {
-    build: {
-      postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-    },
-    css: [
-        '@/assets/css/main.css',
-    ],
-  }
+
+};
