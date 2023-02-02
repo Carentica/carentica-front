8aa4c2cdbe40f2247fbd2d15e462906d1555a2ee
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-02-02 12:24:20 +0100

    adding image in register compo

--- components/home/RegisterSection.vue
+++ components/home/RegisterSection.vue
@@ -19,7 +19,7 @@
                 </div>
                 <div class="-mt-6 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1">
                     <img class="transform translate-x-6 translate-y-6 rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20"
-                        src="images/Screenshot_2022-05-17_at_17.05.12.png" alt="dashboard">
+                        src="../../assets/images/dashboard_screen.png" alt="dashboard">
                 </div>
             </div>
         </div>
