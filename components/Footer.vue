770248da238fb761bd048054dcacae13e58eda71
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-02-02 09:56:22 +0100

    adding footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,131 @@
 <template>
-    <div>
-        Contenu du footer
-    </div>
+    <footer class="bg-gunmetal" aria-labelledby="footer-heading">
+        <h2 id="footer-heading" class="sr-only">Footer</h2>
+        <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+            <div class="lg:flex lg:gap-8 lg:justify-between">
+                <div>
+                    <div class="grid grid-cols-2 gap-8 md:grid-cols-4 md:gap-2">
+                        <div>
+                            <h3 class="text-sm font-semibold text-gray-300 tracking-wider uppercase">Support</h3>
+                            <ul role="list" class="mt-4 space-y-4">
+                                <li>
+                                    <a href="nosformation.html" class="text-base text-gray-200 hover:text-white"> Nos
+                                        formations </a>
+                                </li>
+
+                                <li>
+                                    <a href="contact.html" class="text-base text-gray-200 hover:text-white">
+                                        Contactez-nous </a>
+                                </li>
+
+                                <li>
+                                    <a href="#" class="text-base text-gray-200 hover:text-white"> Documentation </a>
+                                </li>
+
+                                <li>
+                                    <a href="#" class="text-base text-gray-200 hover:text-white"> Guides </a>
+                                </li>
+
+
+                            </ul>
+                        </div>
+                        <div>
+                            <h3 class="text-sm font-semibold text-gray-300 tracking-wider uppercase">
+                                Société</h3>
+                            <ul role="list" class="mt-4 space-y-4">
+                                <li>
+                                    <a href="apropos.html" class="text-base text-gray-200 hover:text-white"> À propos
+                                    </a>
+                                </li>
+
+
+
+                                <li>
+                                    <a href="#" class="text-base text-gray-200 hover:text-white"> Jobs </a>
+                                </li>
+
+                                <li>
+                                    <a href="#" class="text-base text-gray-200 hover:text-white"> Press </a>
+                                </li>
+
+                                <li>
+                                    <a href="#" class="text-base text-gray-200 hover:text-white"> Partners </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-span-2">
+                            <h3 class="text-sm font-semibold text-gray-300 tracking-wider uppercase">Juridique</h3>
+                            <ul role="list" class="mt-4 space-y-4">
+                                <li>
+                                    <a href="#" class="text-base text-gray-200 hover:text-white"> Mentions légales </a>
+                                </li>
+
+                                <li>
+                                    <a href="#" class="text-base text-gray-200 hover:text-white"> Conditions générales
+                                        d'utilisation </a>
+                                </li>
+
+                                <li>
+                                    <a href="#" class="text-base text-gray-200 hover:text-white"> Politique de
+                                        protection des données personnelles </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8 lg:mt-0 ">
+                    <h3 class="text-sm font-semibold text-gray-300 tracking-wider uppercase">Abonnez-vous à notre
+                        newsletter
+                    </h3>
+                    <p class="mt-4 text-base text-gray-200">Les dernières nouvelles envoyés dans votre boîte mail.</p>
+                    <form class="mt-4 sm:flex sm:max-w-md">
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input type="email" name="email-address" id="email-address" autocomplete="email" required
+                            class="appearance-none min-w-0 w-full bg-white border border-transparent rounded-md py-2 px-4 text-base text-gunmetal placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-carmine focus:border-carmine focus:placeholder-gray-400"
+                            placeholder="Entrer votre adresse mail">
+                        <div class="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+                            <button type="submit"
+                                class="w-full bg-white border border-transparent rounded-md py-2 px-4 flex items-center justify-center text-base font-medium text-gunmetal  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-carmine">S'abonner</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="mt-8 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-between">
+                <div class="flex space-x-6 md:order-2">
+
+
+
+
+                    <a href="https://twitter.com/carentica" class="text-gray-300 hover:text-gray-200">
+                        <span class="sr-only">Twitter</span>
+                        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path
+                                d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                        </svg>
+                    </a>
+                    <a href="#" class="fill-gray-300 hover:fill-gray-200">
+                        <span class="sr-only">TikTok</span>
+                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                            <path
+                                d="M448,209.91a210.06,210.06,0,0,1-122.77-39.25V349.38A162.55,162.55,0,1,1,185,188.31V278.2a74.62,74.62,0,1,0,52.23,71.18V0l88,0a121.18,121.18,0,0,0,1.86,22.17h0A122.18,122.18,0,0,0,381,102.39a121.43,121.43,0,0,0,67,20.14Z" />
+                        </svg>
+                    </a>
+
+                    <a href="https://github.com/Carentica" class="text-gray-300 hover:text-gray-200">
+                        <span class="sr-only">GitHub</span>
+                        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                            <path fill-rule="evenodd"
+                                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </a>
+
+
+                </div>
+                <p class="mt-8 text-base text-gray-300 md:mt-0 md:order-1">&copy; 2022 Carentica. Tous droits réservés.
+                </p>
+            </div>
+        </div>
+    </footer>
 </template>
