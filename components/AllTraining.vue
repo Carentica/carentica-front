3bbf2cb5be04817a8058cb405a6fa4ffbd2e042c
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-02-06 16:43:12 +0100

    change css

--- components/AllTraining.vue
+++ components/AllTraining.vue
@@ -7,27 +7,27 @@
                     <img class="h-48 w-full object-cover" :src="training.imageUrl" alt="" />
                 </div>
                 <div class="flex-1 bg-white p-6 flex flex-col justify-between">
-                    <div class="flex-1">
+                    <div class="flex flex-1 flex-col justify-between">
                         <p class="text-sm font-medium text-carmine">
                             <a :href="training.category.href" class="hover:underline">
                                 {{ training.category.name }}
                             </a>
                         </p>
                         <a :href="training.href" class="block mt-2">
-                            <p class="text-xl font-semibold text-gray-900">
-                                {{ training.title }}
-                            </p>
-                            <p class="mt-3 text-base text-gray-500">
-                                {{ training.description }}
-                            </p>
+                                <p class="text-xl font-semibold text-gray-900">
+                                    {{ training.title }}
+                                </p>
+                                <p class="mt-3 text-base text-gray-500 text-justify">
+                                    {{ training.description }}
+                                </p>
                         </a>
-                       
+
                         <div class="mt-5 flex justify-center py-4 sm:mt-0">
                             <a href="cursus.html"
                                 class="flex justify-center items-center px-4 py-2 shadow-sm text-sm font-medium rounded-md text-white bg-carmine hover:bg-gunmetal">
                                 Voir cursus de la formation</a>
                         </div>
-                        
+
                     </div>
                     <div class="mt-6 flex items-center">
                         <div class="flex-shrink-0">
