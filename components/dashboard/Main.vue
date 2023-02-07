6359ab9f63278240aeb5fad848a46b49fa37ebb9
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-02-07 19:21:22 +0100

    new tabs

--- components/dashboard/Main.vue
+++ components/dashboard/Main.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="-mt-32">
+    <section class="">
         <div class="min-h-full">
             <main class="-mt-24 pb-8">
                 <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
--- components/dashboard/Tabs.vue
+++ components/dashboard/Tabs.vue
@@ -1,46 +1,44 @@
 <template>
     <div>
-        <!-- Tabs mobile -->
-        <div class="sm:hidden">
-            <label for="tabs" class="sr-only">Select a tab</label>
-            <select id="tabs" name="tabs"
-                class="block w-full pl-3 pr-10 py-2 text-base border-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
-                <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">{{ tab.name }}</option>
-            </select>
-        </div>
-
-        <!-- Tabks desktop -->
-        <div class="hidden sm:block">
-            <div class="border-b border-gray-500">
-                <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-                    <a v-for="tab in tabs" :key="tab.name" :href="tab.href"
-                        :class="['border-transparent text-white hover:border-carmine', 'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm']"
-                        :aria-current="tab.current ? 'page' : undefined">
-                        {{ tab.name }}
-                    </a>
-                </nav>
-            </div>
-        </div>
-        
-        <DashboardMain />
-
+        <v-tabs>
+            <v-tab v-for="(tab, index) in tabs" v-bind:key="index" @click="selectedTab = index">
+                {{ tab.title }}
+            </v-tab>
+            <template v-for="(tab, index) in tabs">
+                <component :is="tab.component" v-if="selectedTab === index" />
+            </template>
+        </v-tabs>
     </div>
 </template>
 
 <script lang="ts">
-const tabs = [
-    { name: 'Tableau de bord', href: '#', current: true },
-    { name: 'Statistiques', href: '#', current: false },
-    { name: 'Messagerie', href: '#', current: false },
-    { name: 'Profil', href: '#', current: false },
-    { name: 'Paramètre', href: '#', current: false },
-];
-
 export default {
-    setup() {
+    data() {
         return {
-            tabs,
+            selectedTab: 0,
+            tabs: [
+                {   
+                    title: 'Tableau de bord',
+                    component: 'DashboardMain'
+                },
+                {
+                    title: 'Statistiques',
+                    component: 'DashboardMain'
+                },
+                {
+                    title: 'Messagerie',
+                    component: 'DashboardMain'
+                },
+                {
+                    title: 'Profil',
+                    component: 'DashboardMain'
+                },
+                {
+                    title: 'Paramètre',
+                    component: 'DashboardMain'
+                },
+            ]
         }
-    },
+    }
 };
 </script>
